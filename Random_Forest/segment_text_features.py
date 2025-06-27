ab70773e7c43523b2571b52bebec5d35447557dd
Bert & LIWC <81473905+M23A@users.noreply.github.com>
2025-06-27 10:44:17 +0100

    Update segment_text_features.py

--- Random_Forest/segment_text_features.py
+++ Random_Forest/segment_text_features.py
@@ -1,9 +1,7 @@
-
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GroupKFold, cross_val_score
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,confusion_matrix
 from google.colab import drive
@@ -21,7 +19,7 @@ y_combined = data['CAI State'].values  # Labels column
 person_ids = data['PID']
 
 # Initialize Random Forest model
-rf_model = RandomForestClassifier()
+rf_model = RandomForestClassifier(random_state=42)
 
 gkf = GroupKFold(n_splits=10)
 # Store metrics per fold
@@ -36,11 +34,11 @@ true_labels = []
 for train_index, test_index in gkf.split(X_combined, y_combined, groups=person_ids):
     X_train, X_test = X_combined[train_index], X_combined[test_index]
     y_train, y_test = y_combined[train_index], y_combined[test_index]
-    
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    
+
+
     rf_model.fit(X_train_scaled, y_train)
     y_pred = rf_model.predict( X_test_scaled)
 
@@ -60,19 +58,3 @@ print(f"Accuracy:  {np.mean(accuracies):.2f} ± {np.std(accuracies):.2f}")
 print(f"Precision: {np.mean(precisions):.2f} ± {np.std(precisions):.2f}")
 print(f"Recall:    {np.mean(recalls):.2f} ± {np.std(recalls):.2f}")
 print(f"F1 Score:  {np.mean(f1s):.2f} ± {np.std(f1s):.2f}")
-
-
-
-# confusion matrices 
-total_confusion = sum(confusion_matrices)
-print("\nCumulative Confusion Matrix:")
-print(total_confusion)
-
-plt.figure(figsize=(6, 5))
-sns.heatmap(total_confusion, annot=True, fmt='d', cmap='Blues',
-            xticklabels=["Pred 0", "Pred 1"],
-            yticklabels=["True 0", "True 1"])
-plt.title("Cumulative Confusion Matrix (Heatmap)")
-plt.xlabel("Predicted Label")
-plt.ylabel("True Label")
-plt.show()
